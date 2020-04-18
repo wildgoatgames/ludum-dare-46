013f15a73113b61c34a4c7712ae38abed2de2419
Harry <harry@wildgoatgames.co.uk>
2020-04-18 13:44:22 +0100

    Initial Setup (Scenes, Player Movement)
    Started the project and have created scene switching and a player which can move.

---
+++ Assets/Code/Scripts/GameplayManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    
+    public void GameOver()
+    {
+        Debug.Log("Game over!");
+    }
+
+
+}
---
+++ Assets/Code/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header("Script References")]
+    public GameplayManager gameplayManager;
+
+    [Header("Physics")]
+    public Rigidbody2D playerRb;
+    public float movementSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovementController();
+    }
+
+    void MovementController()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            //Debug.Log("Make the ship go up!");
+            playerRb.AddForce(transform.up * movementSpeed, ForceMode2D.Impulse);
+
+        }
+    }
+}
