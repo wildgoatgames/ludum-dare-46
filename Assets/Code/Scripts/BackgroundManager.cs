ee85d804a2f6cccd00dd60696f0795346f957490
Harry <harry@wildgoatgames.co.uk>
2020-04-20 23:21:59 +0100

    Feat: Game Over Screen 2.0
    YEAH

---
+++ Assets/Code/Scripts/BackgroundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("Space Generator").Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
